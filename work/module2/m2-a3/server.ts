6da2a9f588f2ae1fdce12190ac6da749526104a0
arosario513 <a.rosario51304@gmail.com>
2024-10-07 17:00:08 -0400

    Example with bootstrap

---
+++ work/module2/m2-a3/server.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from "express";
+import path from "path";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("src"));
+
+app.use("/css", express.static("node_modules/bootstrap/dist/css"));
+app.use("/js", express.static("./node_modules/bootstrap/dist/js"));
+
+app.get("/", (req: Request, res: Response) =>
+  res.sendFile(path.resolve("src", "index.html")),
+);
+
+app.get("*", (req: Request, res: Response) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(port, () => console.log("Listening on port ", port));
