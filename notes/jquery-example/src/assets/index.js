76846cf9f8f16dd4693dbd2369fe466fe73bdadf
arosario513 <a.rosario51304@gmail.com>
2024-10-28 14:03:16 -0400

    added theme change

--- notes/jquery-example/src/assets/index.js
+++ notes/jquery-example/src/assets/index.js
@@ -1,5 +1,17 @@
 $(() => {
-    $("button").on("click", () => {
-        $("p#txt").toggle();
+    let darkMode = false;
+    $("button#toggle-text").on("click", () => {
+        $(".txt").toggle();
+    });
+    $("button#change-theme").on("click", () => {
+        if (!darkMode) {
+            $("body").css({ "background-color": "#101010", color: "white" });
+            $("main").css({ "background-color": "#202020" });
+            darkMode = true;
+        } else {
+            $("body").css({ "background-color": "#f0f0ff", color: "black" });
+            $("main").css({ "background-color": "white" });
+            darkMode = false;
+        }
     });
 });
