063aad82f9e6039554b213d4a449a7be34616a3f
arosario513 <a.rosario51304@gmail.com>
2024-11-21 19:15:29 -0400

    progress

--- work/final/app/main.py
+++ work/final/app/main.py
@@ -1,10 +1,11 @@
-#!venv/bin/python
+#!venv/bin/python3
 '''Flask App'''
 
 import os
+import re
 from dotenv import load_dotenv
-from flask import Flask
-from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL, MySQLdb
+from flask import Flask, render_template, redirect, url_for, request, session
 
 load_dotenv()
 
@@ -17,13 +18,65 @@ app.config['MYSQL_DB'] = 'data'
 mysql: MySQL = MySQL(app)
 
 
-@app.route("/")
-def index() -> str:
-    cursor = mysql.connection.cursor()
-    cursor.execute("SHOW TABLES;")
-    res = cursor.fetchall()
-    tables = "\n".join(row[0] for row in res)
-    return tables
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    '''Register Function'''
+    msg = ''
+
+    if request.method == 'POST' and 'nombre' in request.form and 'email' in request.form and 'password' in request.form:
+        nombre = request.form['nombre']
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Email is already registered'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+
+            msg = 'Invalid email'
+        elif not nombre or not password or not email:
+            msg = 'Fill the form'
+        else:
+            cursor.execute(
+                'INSERT INTO usuarios (name, email, password) VALUES (%s, %s, %s)', (nombre, email, password,))
+            mysql.connection.commit()
+            msg = 'Registered successfully'
+            return redirect(url_for('login'))
+    if request.method == 'POST':
+        msg = 'Por favor llena el formulario!'
+    return render_template('register.html', msg=msg)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''Login Function'''
+    msg = ''
+
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT * FROM usuarios WHERE email = %s AND password = %s', (email, password,))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['nombre'] = account['nombre']
+            return redirect(url_for('dashboard'))
+    else:
+        msg = 'Invalid Login'
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/logout')
+def logout():
+    '''Logout Function'''
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('nombre', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
